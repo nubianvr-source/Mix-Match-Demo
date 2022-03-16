7ffc821394722a5dacdff96578382294f2b817fd
nubianvr-source <services.nubianvr@gmail.com>
2022-03-16 21:14:45 +0000

    Initial commit

---
+++ Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectionManager : MonoBehaviour
+{
+    //Just Testing
+    public CanvasGroup background;
+
+    public CanvasGroup middle;
+
+    public CanvasGroup foreground;
+
+    public void reset()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+}
