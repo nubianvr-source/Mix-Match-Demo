2ede843fb3ddc50f1c1a3955fe6c1c83aac1afb5
Chisom Ogbonna <101045006+ChisomNubian@users.noreply.github.com>
2022-03-31 18:14:45 +0100

    Test Scene
    The test scene for the demo has been set up...

---
+++ Assets/Scripts/ImageObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Image O", menuName ="Scriptable Objects/Image Object")]
+public class ImageObject : ScriptableObject
+{
+    public Image image;
+
+}
